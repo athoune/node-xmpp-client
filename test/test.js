55903aa0628ec6515d0155a350c3d2f56b4ea975
Mathieu Lecarme <mathieu@garambrogne.net>
2010-08-04 20:29:59 +0200

    Jid as an object

--- test/test.js
+++ test/test.js
@@ -1,16 +1,22 @@
 var sys = require('sys'),
-	client = require('../lib/xmpp-client').Client,
-	jid = require('../lib/xmpp-client').jid;
+	Client = require('../lib/xmpp-client').Client,
+	Jid = require('../lib/xmpp-client').Jid;
 
 exports.testJid = function(test) {
-	var j = jid('mathieu@gtalk.com');
+	var j = new Jid('mathieu@gtalk.com');
 	//sys.debug(JSON.stringify(j));
 	test.equals('mathieu', j.node);
 	test.equals('gtalk.com', j.domain);
 	test.equals(null, j.resource);
-	j = jid('mathieu@jabber.org/node');
+	j = new Jid('mathieu@jabber.org/node');
 	//sys.debug(JSON.stringify(j));
 	test.equals('node', j.resource);
+	test.done();
+};
+
+exports.testClient = function(test) {
+	var c = new Client('mathieu@gtalk.com', 'toto');
+	test.equals('gtalk.com', c.host);
 	test.done();
 };
 
